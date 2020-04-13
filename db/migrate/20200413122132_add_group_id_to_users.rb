902c4ab3857ad5c64b9c97ec031789b2bd8a0d40
EC2 Default User <ec2-user@ip-172-31-42-23.ap-northeast-1.compute.internal>
2020-04-13 13:33:07 +0000

    グループ名を保存するテーブルを作成

--- db/migrate/20200413122132_add_group_id_to_users.rb
+++ db/migrate/20200413122132_add_group_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddGroupIdToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, group_id, :integer
+    add_column :users, :group_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_121839) do
+ActiveRecord::Schema.define(version: 2020_04_13_122132) do
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "key"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_121839) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
