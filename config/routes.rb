8572b4ac55311c84f7920e0296d2888de9d72688
EC2 Default User <ec2-user@ip-172-31-42-23.ap-northeast-1.compute.internal>
2020-04-14 10:06:27 +0000

    質問投稿欄の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "top#index"
   resources :users, only:[:show, :edit, :update]
   resources :groups, only:[:show, :edit, :update]
+  resources :questions, only:[:create]
 end
