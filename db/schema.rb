5d29c7862e390a40b17f0c6cf634206d8b3f3c34
EC2 Default User <ec2-user@ip-172-31-42-23.ap-northeast-1.compute.internal>
2020-04-14 04:42:59 +0000

    ユーザーページの編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_122132) do
+ActiveRecord::Schema.define(version: 2020_04_14_035714) do
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "key"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2020_04_13_122132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "group_id"
+    t.string "family_name"
+    t.string "first_name"
+    t.string "family_name_kana"
+    t.string "first_name_kana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
