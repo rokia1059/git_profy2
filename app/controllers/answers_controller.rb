3d6408c2b9f756a8c0c442b9437990b41f59a3f1
EC2 Default User <ec2-user@ip-172-31-42-23.ap-northeast-1.compute.internal>
2020-04-15 05:53:24 +0000

    質問回答画面表示の実装

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,7 @@
+class AnswersController < ApplicationController
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+    @answer.question_id = @question.id
+  end
+end
