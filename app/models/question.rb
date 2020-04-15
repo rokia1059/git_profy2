a8706f28e923cc3efb9a0b81e914470e14f3d2d0
EC2 Default User <ec2-user@ip-172-31-42-23.ap-northeast-1.compute.internal>
2020-04-15 05:24:00 +0000

    アンサーに関するアソシエーションの実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  has_many   :answers
   
   validates_presence_of :user_id, :text, :group_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
          
   belongs_to :group
   has_many  :questions, -> {order("created_at DESC")}
+  has_many  :answers, -> {order("created_at DESC")}
   
   before_validation :group_key_to_id, if: :has_group_key?
   
